e14e2bfd873b706ba96e4379f9dd6501df3dcf93
AlgoQueen <152845927+AlgoQueen@users.noreply.github.com>
2023-12-19 11:23:07 +0530

    secure

--- fetch_image.py
+++ fetch_image.py
@@ -1,7 +1,16 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the api.env file
+load_dotenv(dotenv_path="api.env")
+
+# Access the API key
+api_key = os.getenv("API_KEY")
+
 
 def fetch_poster(movie_id):
-    response = requests.get(
-        'https://api.themoviedb.org/3/movie/{}?api_key=c4ed22a0b609ff0085b68628115997b9'.format(movie_id))
-    data = response.json()
-    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+   response = requests.get(
+       f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}')
+   data = response.json()
+   return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
